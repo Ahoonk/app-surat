76d7387991e5698e910199ca6a6a113678c4c3ca
Ahoonk <ncyahonk@gmail.com>
2026-05-23 23:15:25 +0700

    naikan hasil print dpi 400

--- resources/views/invoice/show.blade.php
+++ resources/views/invoice/show.blade.php
@@ -37,7 +37,7 @@
 
             try {
                 $imagick = new \Imagick();
-                $imagick->setResolution(300, 300);
+                $imagick->setResolution(400, 400);
                 $imagick->readImage($path . '[0]');
                 $imagick->setImageFormat('png');
 
@@ -59,7 +59,7 @@
                     @unlink($prefix);
 
                     $cmd = escapeshellarg($gsBinary)
-                        . ' -dSAFER -dBATCH -dNOPAUSE -sDEVICE=pngalpha -r300 -dFirstPage=1 -dLastPage=1 -sOutputFile='
+                        . ' -dSAFER -dBATCH -dNOPAUSE -sDEVICE=pngalpha -r400 -dFirstPage=1 -dLastPage=1 -sOutputFile='
                         . escapeshellarg($pngPath) . ' ' . escapeshellarg($path) . ' 2>&1';
 
                     $output = [];
--- resources/views/penawaran/pdf.blade.php
+++ resources/views/penawaran/pdf.blade.php
@@ -50,7 +50,7 @@
 
             try {
                 $imagick = new \Imagick();
-                $imagick->setResolution(300, 300);
+                $imagick->setResolution(400, 400);
                 $imagick->readImage($path . '[0]');
                 $imagick->setImageFormat('png');
 
@@ -72,7 +72,7 @@
                     @unlink($prefix);
 
                     $cmd = escapeshellarg($gsBinary)
-                        . ' -dSAFER -dBATCH -dNOPAUSE -sDEVICE=pngalpha -r300 -dFirstPage=1 -dLastPage=1 -sOutputFile='
+                        . ' -dSAFER -dBATCH -dNOPAUSE -sDEVICE=pngalpha -r400 -dFirstPage=1 -dLastPage=1 -sOutputFile='
                         . escapeshellarg($pngPath) . ' ' . escapeshellarg($path) . ' 2>&1';
 
                     $output = [];
